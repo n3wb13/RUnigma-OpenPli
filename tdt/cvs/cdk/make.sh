c6b10b0febc72e3cf9b681432fc39eb1ce73da1d
piterkadet <piterkadet@piterkadet-VM.(none)>
2012-09-08 19:48:49 +0400

    Корректировка файла make.sh

--- tdt/cvs/cdk/make.sh
+++ tdt/cvs/cdk/make.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ "$1" == -h ] || [ "$1" == --help ]; then
- echo "Parameter 1: target system (1-28)"
- echo "Parameter 2: kernel (1-12)"
- echo "Parameter 3: debug (Y/N)"
- echo "Parameter 4: player (1-3)"
- echo "Parameter 5: Multicom (1-3)"
- echo "Parameter 6: Media Framework (1-2)"
- echo "Parameter 7: External LCD support (1-2)"
+ echo "Parameter 1: Поддерживаемая платформа (1)"
+ echo "Parameter 2: Ядро (1-3)"
+ echo "Parameter 3: Режим отладки (Y/N)"
+ echo "Parameter 4: Плэйер (1)"
+ echo "Parameter 5: Мультиком (1-2)"
+ echo "Parameter 6: Мультимедийная платформа (1-2)"
+ echo "Parameter 7: Поддержка внешнего LCD-дисплея (1-2)"
  echo "Parameter 8: VDR (1-3)"
- echo "Parameter 9: Graphic Framework (1-2)"
+ echo "Parameter 9: Графическая платформа (1-2)"
  exit
 fi
 
@@ -58,140 +58,58 @@ CONFIGPARAM="${CONFIGPARAM} --host=${host_alias} --build=${host_alias}"
 
 ##############################################
 
-echo "Targets:"
-echo " 1) Kathrein UFS-910"
-echo " 3) Kathrein UFS-912"
-echo " 4) Kathrein UFS-922"
-echo " 5) Topfield 7700 HDPVR"
-echo " 6) Fortis based (HDBOX)"
-echo " 7) SpiderBox HL-101"
-echo " 8) Edision Argus vip"
-echo " 9) Cuberevo (IPBOX 9000)"
-echo "10) Cuberevo mini (IPBOX 900)"
-echo "11) Cuberevo mini2 (IPBOX 910)"
-echo "12) Cuberevo 250 (IPBOX 91)"
-echo "13) Cuberevo 9500HD (7000HD)"
-echo "14) Cuberevo 2000HD"
-echo "15) Cuberevo mini_fta (200HD)"
-echo "16) Homecast 5101"
-echo "17) Octagon 1008"
-echo "18) SPARK"
-echo "19) Atevio7500"
-echo "20) SPARK7162"
-echo "21) IPBOX9900"
-echo "22) IPBOX99"
-echo "23) IPBOX55"
-echo "24) Fortis HS7810A"
-echo "25) B4Team ADB 5800S"
-echo "26) Fortis HS7110"
-echo "27) WHITEBOX"
-echo "28) Kathrein UFS-913"
-
+echo "Поддерживаемая платформа:"
+echo "---------------------------------------"
+echo "   1) Opticum 9500 HD (HL-101)"
+echo ""
 case $1 in
-	[1-9] | 1[0-9] | 2[0-9]) REPLY=$1
-	echo -e "\nSelected target: $REPLY\n"
+	[1]) REPLY=$1
+	echo -e "\nВыбранная платформа: $REPLY\n"
 	;;
 	*)
-	read -p "Select target (1-27)? ";;
+	read -p "Выберите платформу (1)? ";;
 esac
 
 case "$REPLY" in
-	 1) TARGET="--enable-ufs910";;
-	 3) TARGET="--enable-ufs912";;
-	 4) TARGET="--enable-ufs922";;
-	 5) TARGET="--enable-tf7700";;
-	 6) TARGET="--enable-fortis_hdbox";;
-	 7) TARGET="--enable-hl101";;
-	 8) TARGET="--enable-vip";;
-	 9) TARGET="--enable-cuberevo";;
-	10) TARGET="--enable-cuberevo_mini";;
-	11) TARGET="--enable-cuberevo_mini2";;
-	12) TARGET="--enable-cuberevo_250hd";;
-	13) TARGET="--enable-cuberevo_9500hd";;
-	14) TARGET="--enable-cuberevo_2000hd";;
-	15) TARGET="--enable-cuberevo_mini_fta";;
-	16) TARGET="--enable-homecast5101";;
-	17) TARGET="--enable-octagon1008";;
-	18) TARGET="--enable-spark";;
-	19) TARGET="--enable-atevio7500";;
-	20) TARGET="--enable-spark7162";;
-	21) TARGET="--enable-ipbox9900";;
-	22) TARGET="--enable-ipbox99";;
-	23) TARGET="--enable-ipbox55";;
-	24) TARGET="--enable-hs7810a";;
-	25) TARGET="--enable-adb_box";;
-	26) TARGET="--enable-hs7110";;
-	27) TARGET="--enable-whitebox";;
-	28) TARGET="--enable-ufs913";;
-	 *) TARGET="--enable-ufs912";;
+	 1) TARGET="--enable-hl101";;
+	 *) TARGET="--enable-hl101";;
 esac
 CONFIGPARAM="$CONFIGPARAM $TARGET"
 
-case "$REPLY" in
-        8) REPLY=$3
-			echo -e "\nModels:"
-			echo " 1) VIP1 v1 [ single tuner + 2 CI + 2 USB ]"
-			echo " 2) VIP1 v2 [ single tuner + 2 CI + 1 USB + plug & play tuner (dvb-s2/t/c) ]"
-			echo " 3) VIP2 v1 [ twin tuner ]"
-
-        	read -p "Select Model (1-3)? "
-
-			case "$REPLY" in
-				1) MODEL="--enable-hl101";;
-				2) MODEL="--enable-vip1_v2";;
-				3) MODEL="--enable-vip2_v1";;
-				*) MODEL="--enable-vip2_v1";;
-			esac
-			CONFIGPARAM="$CONFIGPARAM $MODEL"
-        	;;
-        *)
-esac
-
 ##############################################
-
-echo -e "\nKernel:"
-echo " Maintained:"
-echo "   8) STM 24 P0207 (Recommended)"
-echo "  10) STM 24 P0209"
-echo " Experimental:"
-echo "  11) STM 24 P0210 (UFS910, octagon1008, Fortis based (HDBOX), SPARK, Atevio7500)"
-echo "  12) STM 24 P0211 (UFS910, octagon1008, Cuberevo (IPBOX 9000), Fortis based (HDBOX), SPARK, Atevio7500)"
-echo " Deprecated (Not maintained):"
-echo "   1) STM 22 P0041"
-echo "   2) STM 23 P0119"
-echo "   4) STM 23 P0123"
-echo "   6) STM 24 P0205"
+echo ""
+echo -e "Ядро:"
+echo "---------------------------------------"
+echo "   1) STM 24 P0207"
+echo "   2) STM 24 P0210"
+echo "   3) STM 24 P0211"
+echo ""
 case $2 in
-        [1-9] | 1[0-9]) REPLY=$2
-        echo -e "\nSelected kernel: $REPLY\n"
+        [1-3] | 1[0-9]) REPLY=$2
+        echo -e "\nВыбранное ядро: $REPLY\n"
         ;;
         *)
-        read -p "Select kernel (1-12)? ";;
+        read -p "Выберите ядро (1-3)? ";;
 esac
 
 case "$REPLY" in
-	1)  KERNEL="--enable-stm22 --enable-p0041";;
-	2)  KERNEL="--enable-stm23 --enable-p0119";;
-	4)  KERNEL="--enable-stm23 --enable-p0123";;
-	5)  KERNEL="--enable-stm24 --enable-p0201";STMFB="stm24";;
-	6)  KERNEL="--enable-stm24 --enable-p0205";STMFB="stm24";;
-	7)  KERNEL="--enable-stm24 --enable-p0206";STMFB="stm24";;
-	8)  KERNEL="--enable-stm24 --enable-p0207";STMFB="stm24";;
-	10) KERNEL="--enable-stm24 --enable-p0209";STMFB="stm24";;
-	11) KERNEL="--enable-stm24 --enable-p0210";STMFB="stm24";;
-	12) KERNEL="--enable-stm24 --enable-p0211";STMFB="stm24";;
-	*)  KERNEL="--enable-stm24 --enable-p0207";STMFB="stm24";;
+	1) KERNEL="--enable-stm24 --enable-p0207";STMFB="stm24";;
+	2) KERNEL="--enable-stm24 --enable-p0210";STMFB="stm24";;
+	3) KERNEL="--enable-stm24 --enable-p0211";STMFB="stm24";;
+	*) KERNEL="--enable-stm24 --enable-p0211";STMFB="stm24";;
 esac
 CONFIGPARAM="$CONFIGPARAM $KERNEL"
 
 ##############################################
+echo ""
 if [ "$3" ]; then
- REPLY="$3"
- echo "Activate debug (y/N)? "
- echo -e "\nSelected option: $REPLY\n"
+REPLY="$3"
+echo "   Активировать отладку (y/N)? "
+echo -e "\nВыберите вариант: $REPLY\n"
 else
- REPLY=N
- read -p "Activate debug (y/N)? "
+REPLY=N
+read -p "   Активировать отладку (y/N)? "
+echo "---------------------------------------"
 fi
 [ "$REPLY" == "y" -o "$REPLY" == "Y" ] && CONFIGPARAM="$CONFIGPARAM --enable-debug"
 
@@ -205,85 +123,21 @@ echo "export CONFIG_ZD1211=n"		>> .config
 cd -
 
 ##############################################
-
-echo -e "\nPlayer:"
-echo "   1) Player 131 (Deprecated)"
-echo "   2) Player 179"
-echo "   3) Player 191 (Recommended)"
+echo ""
+echo -e "\nПлэйер:"
+echo "---------------------------------------"
+echo "   1) Плэйер 191"
+echo ""
 case $4 in
-        [1-3]) REPLY=$4
-        echo -e "\nSelected player: $REPLY\n"
+        [1]) REPLY=$4
+        echo -e "\nВыбранный плэйер: $REPLY\n"
         ;;
         *)
-        read -p "Select player (1-3)? ";;
+        read -p "Выберите плэйер (1)? ";;
 esac
 
 case "$REPLY" in
-	1) PLAYER="--enable-player131"
-       cd ../driver/include/
-       if [ -L player2 ]; then
-          rm player2
-       fi
-
-       if [ -L stmfb ]; then
-          rm stmfb
-       fi
-       ln -s player2_131 player2
-       ln -s stmfb_player131 stmfb
-       cd -
-
-       cd ../driver/
-       if [ -L player2 ]; then
-          rm player2
-       fi
-       ln -s player2_131 player2
-       echo "export CONFIG_PLAYER_131=y" >> .config
-       cd -
-
-       cd ../driver/stgfb
-       if [ -L stmfb ]; then
-          rm stmfb
-       fi
-       ln -s stmfb_player131 stmfb
-       cd -
-    ;;
-	2) PLAYER="--enable-player179"
-       cd ../driver/include/
-       if [ -L player2 ]; then
-          rm player2
-       fi
-
-       if [ -L stmfb ]; then
-          rm stmfb
-       fi
-       ln -s player2_179 player2
-       if [ "$STMFB" == "stm24" ]; then
-           ln -s stmfb-3.1_stm24_0102 stmfb
-       else
-           ln -s stmfb-3.1_stm23_0032 stmfb
-       fi
-       cd -
-
-       cd ../driver/
-       if [ -L player2 ]; then
-          rm player2
-       fi
-       ln -s player2_179 player2
-       echo "export CONFIG_PLAYER_179=y" >> .config
-       cd -
-
-       cd ../driver/stgfb
-       if [ -L stmfb ]; then
-          rm stmfb
-       fi
-       if [ "$STMFB" == "stm24" ]; then
-           ln -s stmfb-3.1_stm24_0102 stmfb
-       else
-           ln -s stmfb-3.1_stm23_0032 stmfb
-       fi
-       cd -
-    ;;
-	3) PLAYER="--enable-player191"
+	1) PLAYER="--enable-player191"
        cd ../driver/include/
        if [ -L player2 ]; then
           rm player2
@@ -319,42 +173,25 @@ case "$REPLY" in
        fi
        cd -
     ;;
-	*) PLAYER="--enable-player131";;
+	*) PLAYER="--enable-player191";;
 esac
 
 ##############################################
-
-echo -e "\nMulticom:"
-echo "   1) Multicom 3.2.2     (Recommended for Player179)"
-echo "   3) Multicom 3.2.4     (Recommended for Player191)"
+echo ""
+echo -e "\nМультиком:"
+echo "---------------------------------------"
+echo "   1) Мультиком 3.2.4 "
+echo ""
 case $5 in
-        [1-3]) REPLY=$5
-        echo -e "\nSelected multicom: $REPLY\n"
+        [1]) REPLY=$5
+        echo -e "\nВыбранный мультиком: $REPLY\n"
         ;;
         *)
-        read -p "Select multicom (1-3)? ";;
+        read -p "Выберите мультиком (1)? ";;
 esac
-
+echo ""
 case "$REPLY" in
-	1) MULTICOM="--enable-multicom322"
-       cd ../driver/include/
-       if [ -L multicom ]; then
-          rm multicom
-       fi
-
-       ln -s multicom-3.2.2 multicom
-       cd -
-
-       cd ../driver/
-       if [ -L multicom ]; then
-          rm multicom
-       fi
-
-       ln -s multicom-3.2.2 multicom
-       echo "export CONFIG_MULTICOM322=y" >> .config
-       cd -
-    ;;
-	2 | 3) MULTICOM="--enable-multicom324"
+       1) MULTICOM="--enable-multicom324"
        cd ../driver/include/
        if [ -L multicom ]; then
           rm multicom
@@ -372,20 +209,22 @@ case "$REPLY" in
        echo "export CONFIG_MULTICOM324=y" >> .config
        cd -
     ;;
-	*) MULTICOM="--enable-multicom322";;
+	*) MULTICOM="--enable-multicom324";;
 esac
 
 ##############################################
-
-echo -e "\nMedia Framework:"
-echo "   1) eplayer3  (Recommended for Enigma1/2, Neutrino/HD, VDR)"
-echo "   2) gstreamer (Recommended for Enigma2 / PLI, XBMC)"
+echo ""
+echo -e "\nМультимедийная платформа:"
+echo "---------------------------------------"
+echo "   1) eplayer3  (Рекомендуется для Enigma1/2, Neutrino/HD, VDR)"
+echo "   2) gstreamer (Рекомендуется для Enigma2 / PLI, XBMC)"
+echo ""
 case $6 in
         [1-2]) REPLY=$6
-        echo -e "\nSelected media framwork: $REPLY\n"
+        echo -e "\nВыбранная мультимедийная платформа: $REPLY\n"
         ;;
         *)
-        read -p "Select media framwork (1-2)? ";;
+        read -p "Выберите мультимедийную платформу (1-2)? ";;
 esac
 
 case "$REPLY" in
@@ -395,16 +234,18 @@ case "$REPLY" in
 esac
 
 ##############################################
-
-echo -e "\nExternal LCD support:"
-echo "   1) No external LCD"
-echo "   2) graphlcd for external LCD"
+echo ""
+echo -e "\nПоддержка внешнего LCD-дисплея:"
+echo "---------------------------------------"
+echo "   1) С поддержкой LCD-дисплея"
+echo "   2) Без поддержки LCD-дисплея"
+echo ""
 case $7 in
         [1-2]) REPLY=$7
-        echo -e "\nSelected LCD support: $REPLY\n"
+        echo -e "\nВыбранный режим: $REPLY\n"
         ;;
         *)
-        read -p "Select external LCD support (1-2)? ";;
+        read -p "Выберите режим поддержки LCD-дисплея (1-2)? ";;
 esac
 
 case "$REPLY" in
@@ -418,17 +259,19 @@ esac
 CONFIGPARAM="$CONFIGPARAM $PLAYER $MULTICOM $MEDIAFW $EXTERNAL_LCD"
 
 ##############################################
-
-echo -e "\nVDR-1.7.XX:"
-echo "   1) No"
+echo ""
+echo -e "\nВыбрать тип VDR:"
+echo "---------------------------------------"
+echo "   1) НЕТ"
 echo "   2) VDR-1.7.22"
 echo "   3) VDR-1.7.27"
+echo ""
 case $8 in
 	[1-3]) REPLY=$8
-        echo -e "\nSelected VDR-1.7.XX: $REPLY\n"
+        echo -e "\nВыбранный тип VDR: $REPLY\n"
         ;;
         *)
-        read -p "Select VDR-1.7.XX (1-3)? ";;
+        read -p "Выберитое тип VDR (1-3)? ";;
 esac
 case "$REPLY" in
 	1) VDR=""
@@ -456,20 +299,22 @@ case "$REPLY" in
        ln -s vdr-1.7.27 vdr
        cd -
     ;;
-	*) VDR="--enable-vdr1722";;
+	*) VDR="--enable-vdr1727";;
 esac
 
 ##############################################
-
-echo -e "\nGraphic Framework:"
-echo "   1) Framebuffer (Enigma1/2, Neutrino1/HD, VDR)"
-echo "   2) DirectFB    (XBMC for UFS912, UFS913, Atevio7500)"
+echo ""
+echo -e "\nГрафическая платформа:"
+echo "---------------------------------------"
+echo "   1) Framebuffer (Рекомендуется для Enigma1/2, Neutrino1/HD, VDR)"
+echo "   2) DirectFB    (Рекомендуется для XBMC)"
+echo ""
 case $9 in
         [1-2]) REPLY=$9
-        echo -e "\nSelected Graphic Framework: $REPLY\n"
+        echo -e "\nВыбранная графическая платформа: $REPLY\n"
         ;;
         *)
-        read -p "Select Graphic Framework (1-2)? ";;
+        read -p "Выберите графическую платформу (1-2)? ";;
 esac
 
 case "$REPLY" in
@@ -484,6 +329,7 @@ CONFIGPARAM="$CONFIGPARAM $PLAYER $MULTICOM $MEDIAFW $EXTERNAL_LCD $VDR $GFW"
 
 ##############################################
 
+
 echo && \
 echo "Performing autogen.sh..." && \
 echo "------------------------" && \
@@ -497,7 +343,8 @@ echo && \
 #Dagobert: I find it sometimes useful to know
 #what I have build last in this directory ;)
 echo $CONFIGPARAM >lastChoice
-
+echo ""
+echo ""
 echo "-----------------------"
 echo "Your build enivroment is ready :-)"
 echo "Your next step could be:"
@@ -509,3 +356,4 @@ echo "make yaud-vdr"
 echo "make yaud-enigma1-hd"
 echo "make yaud-xbmc-nightly"
 echo "-----------------------"
+echo ""
